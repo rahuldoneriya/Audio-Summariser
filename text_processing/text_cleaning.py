c3606b6f8653ae63589f68ab2a37e7e560e9f13a
rahuldoneriya <doneriya.rahul1997@gmail.com>
2021-05-12 18:36:12 +0000

    adding regex

--- text_processing/text_cleaning.py
+++ text_processing/text_cleaning.py
@@ -4,4 +4,5 @@ def clean_text(article_text):
     article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
     article_text = re.sub(r'\s+', ' ', article_text)
     formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text)
+    formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
     return formatted_article_text
