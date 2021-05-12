0a3ae1e5895999cd605ec393862a1e86cae16293
rahuldoneriya <doneriya.rahul1997@gmail.com>
2021-05-12 19:36:12 +0000

    add logic to delete blob

--- audio_processing/audio_upload.py
+++ audio_processing/audio_upload.py
@@ -3,3 +3,9 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
     blob.upload_from_filename(source_file_name)
+
+def delete_blob(bucket_name, blob_name):
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.delete()
